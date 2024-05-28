231ba0c7bee8719721582e61e130718dc3a3bdbd
Angie Plumb <angie@Angies-MacBook-Air-2.local>
2024-05-28 14:34:49 -0700

    updated grade file

--- grade.sh
+++ grade.sh
@@ -8,9 +8,26 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -e '/Users/angie/list-examples-grader/student-submission/ListExamples.java' ]]
+then
+  echo "Found File"
+else
+    echo "File isn't correct"
+fi
+
+cp student-submission/ListExamples.java grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+set -e
+cd grading-area/
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+echo $?
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
